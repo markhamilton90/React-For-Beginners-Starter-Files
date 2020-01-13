0746c8d78fda59a9ce8b6339e041522f629f3cb6
markhamilton90 <msh387@gmail.com>
2020-01-12 22:47:19 -0600

    updating getFunName helper function to include adverbs, importing it into StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
     render() {
         return (
             <form action="" className="store-selector">
-                {/* Comment */}
                 <h2>Please Enter A Store</h2>
-                <input type="text" placeholder="Store Name" required />
+                <input type="text" placeholder="Store Name" defaultValue={ getFunName() } required />
                 <button type="submit">Visit Store</button>
             </form>
         )
--- catch-of-the-day/src/helpers.js
+++ catch-of-the-day/src/helpers.js
@@ -1,16 +1,16 @@
 export function formatPrice(cents) {
-  return (cents / 100).toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD"
-  });
+    return (cents / 100).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
 }
 
 export function rando(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
 export function slugify(text) {
-  return text
+    return text
     .toString()
     .toLowerCase()
     .replace(/\s+/g, "-")
@@ -21,77 +21,115 @@ export function slugify(text) {
 }
 
 export function getFunName() {
-  const adjectives = [
-    "adorable",
-    "beautiful",
-    "clean",
-    "drab",
-    "elegant",
-    "fancy",
-    "glamorous",
-    "handsome",
-    "long",
-    "magnificent",
-    "old-fashioned",
-    "plain",
-    "quaint",
-    "sparkling",
-    "ugliest",
-    "unsightly",
-    "angry",
-    "bewildered",
-    "clumsy",
-    "defeated",
-    "embarrassed",
-    "fierce",
-    "grumpy",
-    "helpless",
-    "itchy",
-    "jealous",
-    "lazy",
-    "mysterious",
-    "nervous",
-    "obnoxious",
-    "panicky",
-    "repulsive",
-    "scary",
-    "thoughtless",
-    "uptight",
-    "worried"
-  ];
+    const adverbs = [
+        "existentially",
+        "diabolically",
+        "beautifully",
+        "disgustingly",
+        "moronically",
+        "amazingly",
+        "exasperatingly",
+        "handsomely",
+        "boringly",
+        "magnificently",
+        "dreadfully",
+        "painstakingly",
+        "uneasily",
+        "sparklingly",
+        "loudly",
+        "unsightly",
+        "angrily",
+        "terribly",
+        "clumsily",
+        "defeatedly",
+        "dangerously",
+        "absolutely",
+        "dashingly",
+        "helplessly",
+        "badly",
+        "obnoxiously",
+        "tentatively",
+        "mysteriously",
+        "nervously",
+        "obnoxiously",
+        "fecetiously",
+        "morosely",
+        "scarily",
+        "thoughtlessly",
+        "uptightly",
+        "worriedly"
+    ];
+    const adjectives = [
+        "adorable",
+        "beautiful",
+        "clean",
+        "perturbed",
+        "elegant",
+        "fancy",
+        "glamorous",
+        "handsome",
+        "long",
+        "magnificent",
+        "old-fashioned",
+        "lit",
+        "quaint",
+        "sparkling",
+        "ugly",
+        "unsightly",
+        "angry",
+        "bewildered",
+        "clumsy",
+        "defeated",
+        "embarrassed",
+        "fierce",
+        "obligatory",
+        "helpless",
+        "itchy",
+        "jealous",
+        "lazy",
+        "mysterious",
+        "nervous",
+        "obnoxious",
+        "panicky",
+        "repulsive",
+        "scary",
+        "thoughtless",
+        "uptight",
+        "worried"
+    ];
 
-  const nouns = [
-    "women",
-    "men",
-    "children",
-    "teeth",
-    "feet",
-    "people",
-    "leaves",
-    "mice",
-    "geese",
-    "halves",
-    "knives",
-    "wives",
-    "lives",
-    "elves",
-    "loaves",
-    "potatoes",
-    "tomatoes",
-    "cacti",
-    "foci",
-    "fungi",
-    "nuclei",
-    "syllabuses",
-    "analyses",
-    "diagnoses",
-    "oases",
-    "theses",
-    "crises",
-    "phenomena",
-    "criteria",
-    "data"
-  ];
+    const nouns = [
+        "women",
+        "men",
+        "children",
+        "teeth",
+        "feet",
+        "people",
+        "leaves",
+        "mice",
+        "geese",
+        "spoons",
+        "knives",
+        "wives",
+        "sporks",
+        "elves",
+        "loaves",
+        "potatoes",
+        "tomatoes",
+        "cacti",
+        "scientists",
+        "fungi",
+        "biscuits",
+        "tourists",
+        "visions",
+        "diagnoses",
+        "gnomes",
+        "leprechauns",
+        "cashews",
+        "punishments",
+        "criteria",
+        "data"
+    ];
 
-  return `${rando(adjectives)}-${rando(adjectives)}-${rando(nouns)}`;
+    return `${rando(adverbs)}-${rando(adjectives)}-${rando(nouns)}`;
 }
