86375c40e90fba55cb61bf39b8ada87708197fe6
markhamilton90 <msh387@gmail.com>
2020-01-14 00:27:27 -0600

    adding functionality to StorePicker form

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = e => {
+        e.preventDefault();
+        const storeName = this.myInput.current.value;
+        this.props.history.push(`/store/${storeName}`);
+    }
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" placeholder="Store Name" defaultValue={ getFunName() } required />
+                <input
+                    ref={this.myInput}
+                    type="text"
+                    placeholder="Store Name"
+                    defaultValue={ getFunName() }
+                    required />
                 <button type="submit">Visit Store</button>
             </form>
         )
