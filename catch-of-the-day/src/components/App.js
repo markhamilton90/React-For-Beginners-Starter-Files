a12d28f8796caecd1d6284d9942c875c7d0cb0c1
markhamilton90 <msh387@gmail.com>
2020-02-13 22:25:12 -0600

    fish can be added to order and order displays the total price

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
         fishes[`fish${Date.now()}`] = fish;
         // overwrite existing state and trigger change in React
         this.setState({
-            // fishes: fishes (if they are the same, you can do the below)
+            // fishes: fishes
             fishes
         });
     }
@@ -26,16 +26,27 @@ class App extends React.Component {
             fishes: sampleFishes
         });
     }
+    addToOrder = (key) => {
+        const order = { ...this.state.order };
+        order[key] = order[key] + 1 || 1;
+        this.setState({ order });
+    }
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+                        {Object.keys(this.state.fishes).map(key => (
+                            <Fish
+                                key={key}
+                                index={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder} />
+                        ))}
                     </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
             </div>
         )
